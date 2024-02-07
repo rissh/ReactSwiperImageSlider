7e84982ecc8318399cfbc490272fd807b69b0e43
Rishikesh <rishikeshjagadale19@gmail.com>
2024-02-07 20:28:29 +0530

    Final Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import HomeSlider from "./components/HomeSlider";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is React Swiper</h1>
+      <HomeSlider />
+    </div>
+  );
+}
+
+export default App;
